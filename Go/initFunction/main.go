1eb6ac6d0da5e89602747b5dc702be69a8b943af
coldinke <yangzhibo.yang@gmail.com>
2025-03-06 11:21:38 +0800

    Go: Add init function example

---
+++ Go/initFunction/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("This is first init function")
+}
+
+func init() {
+	fmt.Println("This is second init function")
+}
+
+func main() {
+	fmt.Println("vim-go from main")
+}
+
+func init() {
+	fmt.Println("This is three init function")
+}
